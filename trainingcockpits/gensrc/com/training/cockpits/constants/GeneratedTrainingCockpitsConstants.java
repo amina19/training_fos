208efbecc4a1381df156cce7c12c9b3376c59ce5
ameena <ameena.siddiq@gmail.com>
2018-10-26 17:03:36 +0530

    initial commit

---
+++ trainingcockpits/gensrc/com/training/cockpits/constants/GeneratedTrainingCockpitsConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 22 Oct, 2018 12:07:41 PM                    ---
+ * ----------------------------------------------------------------
+ */
+package com.training.cockpits.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedTrainingCockpitsConstants
+{
+	public static final String EXTENSIONNAME = "trainingcockpits";
+	
+	protected GeneratedTrainingCockpitsConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ traininginitialdata/gensrc/com/training/initialdata/constants/GeneratedTrainingInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 22 Oct, 2018 12:07:41 PM                    ---
+ * ----------------------------------------------------------------
+ */
+package com.training.initialdata.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedTrainingInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "traininginitialdata";
+	
+	protected GeneratedTrainingInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
